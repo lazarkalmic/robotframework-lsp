96e405a0c363d8b6fff83fee4c1bbccafa663cb7
Fabio Zadrozny <fabiofz@gmail.com>
2023-10-31 11:30:01 -0300

    Creating WindowsInspector (base API for windows inspector in Robocorp Code).

--- robocorp-code/src/robocorp_code/inspector/windows/windows_inspector.py
+++ robocorp-code/src/robocorp_code/inspector/windows/windows_inspector.py
@@ -1,49 +1,115 @@
+import enum
 import typing
-from typing import Any, Callable, List, Literal, Tuple, TypedDict
+from typing import Any, Callable, List, Literal, Optional, Set, TypedDict, Union
 
 from robocorp_ls_core.callbacks import Callback
 
-
-class ControlLocatorInfoTypedDict(TypedDict):
-    control: str
-    class_name: str  # Referenced as 'class' in the locator
-    name: str
-    automation_id: str
-    handle: int
-    left: int
-    right: int
-    top: int
-    bottom: int
-    width: int
-    height: int
+if typing.TYPE_CHECKING:
+    from robocorp_code.inspector.windows.robocorp_windows import (
+        ControlElement,
+        WindowElement,
+    )
+    from robocorp_code.inspector.windows.robocorp_windows._iter_tree import (
+        ControlTreeNode,
+    )
 
 
-class WindowLocatorInfoTypedDict(TypedDict):
-    # Same as control
-    control: str
-    class_name: str  # Referenced as 'class' in the locator
-    name: str
-    automation_id: str
-    handle: int
-    left: int
-    right: int
-    top: int
-    bottom: int
-    width: int
-    height: int
+ControlLocatorInfoTypedDict = TypedDict(
+    "ControlLocatorInfoTypedDict",
+    {
+        "control": str,
+        "class": str,
+        "name": str,
+        "automation_id": str,
+        "handle": int,
+        "left": int,
+        "right": int,
+        "top": int,
+        "bottom": int,
+        "width": int,
+        "height": int,
+        "xcenter": int,
+        "ycenter": int,
+        # Relative information based on the query.
+        # May be used to recreate the tree-structure
+        # afterwards (as APIs usually provide
+        # a flat list).
+        "depth": int,
+        "child_pos": int,
+        "path": str,
+    },
+)
 
-    # Additional
-    pid: int
-    executable: str
+
+WindowLocatorInfoTypedDict = TypedDict(
+    "WindowLocatorInfoTypedDict",
+    {
+        # Same as control
+        "control": str,
+        "class": str,
+        "name": str,
+        "automation_id": str,
+        "handle": int,
+        "left": int,
+        "right": int,
+        "top": int,
+        "bottom": int,
+        "width": int,
+        "height": int,
+        "xcenter": int,
+        "ycenter": int,
+        # Note: depth/child_pos/path don't make sense for
+        # top-level windows.
+        # Additional only in Window
+        "pid": int,
+        "executable": str,
+    },
+)
 
 
-class TreeNodeTypedDict(TypedDict):
-    data: ControlLocatorInfoTypedDict
-    children: List["TreeNodeTypedDict"]
+class MatchesAndHierarchy(TypedDict):
+    # A list with the `path` items matched (these are the ones that the
+    # locator matched)
+    matched_paths: List[str]
+    # This includes all the entries found along with the full hierarchy
+    # to reach the matched entries.
+    hierarchy: List[ControlLocatorInfoTypedDict]
+
+
+def _get_from_obj(el: Union["WindowElement", "ControlElement"], dct_name: str) -> Any:
+    from robocorp_code.inspector.windows.robocorp_windows._ui_automation_wrapper import (
+        LocationInfo,
+    )
+
+    attr_name = dct_name
+    target: Union["WindowElement", "ControlElement", "LocationInfo"] = el
+    if attr_name == "control":
+        attr_name = "control_type"
+    elif attr_name == "class":
+        attr_name = "class_name"
+
+    elif dct_name in ("depth", "child_pos", "path"):
+        target = el.location_info
+
+    return getattr(target, attr_name)
+
+
+def to_window_info(w: "WindowElement") -> WindowLocatorInfoTypedDict:
+    ret = {}
+    for dct_name in WindowLocatorInfoTypedDict.__annotations__:
+        ret[dct_name] = _get_from_obj(w, dct_name)
+    return typing.cast(WindowLocatorInfoTypedDict, ret)
+
+
+def to_control_info(el: "ControlElement") -> ControlLocatorInfoTypedDict:
+    ret = {}
+    for dct_name in ControlLocatorInfoTypedDict.__annotations__:
+        ret[dct_name] = _get_from_obj(el, dct_name)
+    return typing.cast(ControlLocatorInfoTypedDict, ret)
 
 
 class IOnPickCallback(typing.Protocol):
-    def __call__(self, locator_info_tree: Tuple[ControlLocatorInfoTypedDict]):
+    def __call__(self, locator_info_tree: List[ControlLocatorInfoTypedDict]):
         """
         Args:
             locator_info_tree: This will provide the structure from parent to
@@ -53,22 +119,76 @@ class IOnPickCallback(typing.Protocol):
         """
 
     def register(
-        self, callback: Callable[[Tuple[ControlLocatorInfoTypedDict]], Any]
+        self, callback: Callable[[List[ControlLocatorInfoTypedDict]], Any]
     ) -> None:
         pass
 
     def unregister(
-        self, callback: Callable[[Tuple[ControlLocatorInfoTypedDict]], Any]
+        self, callback: Callable[[List[ControlLocatorInfoTypedDict]], Any]
     ) -> None:
         pass
 
 
+class _State(enum.Enum):
+    default = 1
+    picking = 2
+    highlighting = 3
+
+
 class WindowsInspector:
     def __init__(self) -> None:
+        from robocorp_code.inspector.windows.robocorp_windows._inspect import (
+            ElementInspector,
+        )
+
         # Called as: self.on_pick([ControlLocatorInfoTypedDict])
         self.on_pick: IOnPickCallback = Callback()
+        self._element_inspector: Optional[ElementInspector] = None
+        self._state = _State.default
 
-    def start_pick(self, window_locator: str) -> None:
+    def dispose(self):
+        if self._element_inspector is not None:
+            self._element_inspector.dispose()
+            self._element_inspector = None
+        self._state = _State.default
+
+    def set_window_locator(self, window_locator: str):
+        """
+        Sets the base window which should be inspected.
+
+        Args:
+            window_locator: The locator of the window which should be inspected.
+
+        Raises:
+            ElementNotFound if the window matching the given locator wasn't found.
+        """
+        self.dispose()
+
+        from robocorp_code.inspector.windows.robocorp_windows import find_window
+        from robocorp_code.inspector.windows.robocorp_windows._inspect import (
+            ElementInspector,
+        )
+
+        pick_window = find_window(window_locator)
+        self._element_inspector = ElementInspector(pick_window)
+
+    def _on_internal_pick(self, found: List["ControlTreeNode[ControlElement]"]):
+        converted: List[ControlLocatorInfoTypedDict] = []
+        for node in found:
+            converted.append(to_control_info(node.control))
+        self.on_pick(converted)
+
+    def reset_to_default_state(self):
+        """
+        Will stop any pick or highlight currently in place.
+        """
+        if self._state == _State.picking:
+            self.stop_pick()
+
+        elif self._state == _State.highlighting:
+            self.stop_highlight()
+
+    def start_pick(self) -> None:
         """
         Starts picking so that when the cursor is hovered over an item of the
         UI the `on_pick` callback is triggered.
@@ -79,18 +199,28 @@ class WindowsInspector:
         Raises:
             ElementNotFound if the window matching the given locator wasn't found.
         """
+        if self._element_inspector is None:
+            return
+
+        self.reset_to_default_state()
+        self._state = _State.picking
+        self._element_inspector.start_picking(self._on_internal_pick)
 
     def stop_pick(self) -> None:
         """
         Stops picking.
         """
+        if self._state == _State.picking:
+            if self._element_inspector is not None:
+                self._element_inspector.stop_picking()
+            self._state = _State.default
 
-    def start_highlight_matches(
+    def start_highlight(
         self,
         locator: str,
         search_depth: int = 8,
         search_strategy: Literal["siblings", "all"] = "all",
-    ) -> List[TreeNodeTypedDict]:
+    ) -> MatchesAndHierarchy:
         """
         Starts highlighting the matches given by the locator specified.
 
@@ -98,19 +228,73 @@ class WindowsInspector:
             locator: The locator whose matches should be highlighted.
 
         Returns:
-            The matches found as a tree hierarchy. Note that while picking
-            will only give a single tree path, this needs an actual tree structure
-            as siblings may be found.
-
-            Note: there's no root element, the first level children of the
-            window or element are directly given.
+            The matches found as a flattened list (the tree hierarchy
+            may be rebuilt based on the `path` or `depth` and `child_pos`).
         """
-        return []
+        if not self._element_inspector:
+            raise RuntimeError(
+                "Unable to make match because `set_window_locator` was not previously used to set the window of interest."
+            )
+        from robocorp_code.inspector.windows.robocorp_windows._inspect import (
+            build_parent_hierarchy,
+        )
 
-    def stop_highlight_matches(self) -> None:
+        matched_controls = self._element_inspector.start_highlight(
+            locator,
+            search_depth=search_depth,
+            timeout=0,
+            search_strategy=search_strategy,
+        )
+
+        hierarchy: List[ControlLocatorInfoTypedDict] = []
+        matched_paths: List[str] = []
+
+        # Keep the paths already seen.
+        already_in_hierarchy: Set[str] = set()
+        for control in matched_controls:
+            path = control.location_info.path
+            assert path
+            matched_paths.append(path)
+
+            for node in build_parent_hierarchy(
+                control, self._element_inspector.control_element
+            ):
+                if node.path not in already_in_hierarchy:
+                    already_in_hierarchy.add(node.path)
+                    hierarchy.append(to_control_info(node.control))
+
+        return {"matched_paths": matched_paths, "hierarchy": hierarchy}
+
+    def stop_highlight(self) -> None:
         """
         Stops highlighting matches.
         """
+        if self._state == _State.highlighting:
+            if self._element_inspector is not None:
+                self._element_inspector.stop_highlight()
+            self._state = _State.default
 
     def list_windows(self) -> List[WindowLocatorInfoTypedDict]:
-        return []
+        from robocorp_code.inspector.windows import robocorp_windows
+
+        windows = robocorp_windows.find_windows("regex:.*", search_depth=1)
+        return [to_window_info(w) for w in windows]
+
+    def collect_tree(
+        self,
+        locator: str,
+        search_depth: int = 8,
+        search_strategy: Literal["siblings", "all"] = "all",
+    ) -> List[ControlLocatorInfoTypedDict]:
+        if self._element_inspector is None:
+            return []
+
+        ret: List[ControlLocatorInfoTypedDict] = []
+        for el in self._element_inspector.control_element.find_many(
+            locator,
+            search_depth=search_depth,
+            search_strategy=search_strategy,
+            timeout=0,
+        ):
+            ret.append(to_control_info(el))
+        return ret
--- robocorp-code/tests/robocorp_code_tests/inspector/test_windows_inspector.py
+++ robocorp-code/tests/robocorp_code_tests/inspector/test_windows_inspector.py
@@ -1,4 +1,5 @@
 import subprocess
+import threading
 from typing import Iterator
 
 import pytest
@@ -44,15 +45,52 @@ def windows_inspector(tk_process) -> Iterator["WindowsInspector"]:
     windows_inspector = WindowsInspector()
 
     yield windows_inspector
+    windows_inspector.dispose()
 
 
 def test_windows_inspector(windows_inspector: WindowsInspector) -> None:
+    from robocorp_code.inspector.windows.robocorp_windows._vendored.uiautomation.uiautomation import (
+        SetCursorPos,
+    )
+
     windows = windows_inspector.list_windows()
     for window in windows:
-        if window.name == "Tkinter Elements Showcase":
+        if window["name"] == "Tkinter Elements Showcase":
             break
     else:
         raise AssertionError("Did not find tkinter window.")
 
-    windows_inspector.start_pick('name:"Tkinter Elements Showcase"')
-    windows_inspector.start_highlight_matches("type:Button")
+    assert window["pid"]
+    assert window["name"]
+    windows_inspector.set_window_locator(f"name:{window['name']}")
+    ev = threading.Event()
+
+    found_elements = []
+
+    def on_pick(found):
+        found_elements.append(found)
+        ev.set()
+
+    try:
+        windows_inspector.on_pick.register(on_pick)
+        tree_elements = windows_inspector.collect_tree(f"control:Button")
+        assert len(tree_elements) == 10
+
+        bt = tree_elements[0]
+        x, y = bt["xcenter"], bt["ycenter"]
+
+        windows_inspector.start_pick()
+
+        SetCursorPos(x, y)
+        ev.wait(5)
+        assert found_elements
+        found = found_elements[-1][-1]
+        assert found["path"] == bt["path"]
+
+        # Should automatically stop the pick and start the highlight
+        highlighted = windows_inspector.start_highlight(f"path:{bt['path']}")
+        assert [bt["path"]] == highlighted["matched_paths"]
+
+    finally:
+        # Stops picking and highlight.
+        windows_inspector.reset_to_default_state()
